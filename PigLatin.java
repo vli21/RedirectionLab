c0f1806eba02c596349bed81dcfd1597219655d8
vli21 <vli21@stuy.edu>
2021-01-04 16:05:30 -0500

    Template for PigLatin

---
+++ PigLatin.java
@@ -0,0 +1,11 @@
+public class PigLatin{
+  public static String pigLatinSimple(String s){
+    return "";
+  }
+  public static String pigLatin(String s){
+    return "";
+  }
+  public static String pigLatinBest(String s){
+    return "";
+  }
+}
