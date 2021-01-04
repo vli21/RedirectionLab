8b2de432437147568626cbefd80e4794b232742d
vli21 <vli21@stuy.edu>
2021-01-04 15:51:37 -0500

    MakeStars prints out text as stars

--- MakeStars.java
+++ MakeStars.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MakeStars{
   public static String toStars(String str){
     String copy="";
@@ -11,5 +12,22 @@ public class MakeStars{
     }
     return copy;
   }
-  
+  public static void main (String []args){
+    //use the standard input (terminal input)
+    //as the string that you read from
+    Scanner line = new Scanner( System.in );
+    //use hasNextLine()/nextLine() to loop over
+    //all of the data
+    //If you want to read the input word by word
+    //this can be replaced with hasNext() and next()
+    //do something with line
+    while(line.hasNextLine()){
+      Scanner word = new Scanner(line.nextLine());
+
+      while(word.hasNext()){
+        System.out.print(toStars(word.next())+" ");
+      }
+      System.out.println();
+    }
+  }
 }
