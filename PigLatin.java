587574e05a98770892964ea5acf8137a31d08b51
vli21 <vli21@stuy.edu>
2021-01-04 16:31:41 -0500

    PigLatinSimple works

--- PigLatin.java
+++ PigLatin.java
@@ -1,6 +1,20 @@
 public class PigLatin{
+
   public static String pigLatinSimple(String s){
-    return "";
+    String pigged="";
+      if (s.length()==0){
+        return "";
+      }
+      else{
+        char c= s.charAt(0);
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+          pigged= s+"hay";
+        }
+        else{
+          pigged= s.substring(1,s.length())+s.charAt(0)+"ay";
+        }
+      }
+    return pigged;
   }
   public static String pigLatin(String s){
     return "";
@@ -8,5 +22,11 @@ public class PigLatin{
   public static String pigLatinBest(String s){
     return "";
   }
-  public static void main (String []args){}
+  public static void main (String []args){
+  /*  System.out.println(pigLatinSimple("mock")); //→ "ockmay"
+    System.out.println(pigLatinSimple("pie")); //→ "iepay"
+    System.out.println(pigLatinSimple("david")); //→ "avidday"
+    System.out.println(pigLatinSimple("aaron")); //→ "aaronhay"
+    */
+  }
 }
