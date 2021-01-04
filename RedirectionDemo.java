0f2e833baae89e4ead927b5ead59fc8e75203b99
vli21 <vli21@stuy.edu>
2021-01-04 15:38:58 -0500

    reads it word by word

--- RedirectionDemo.java
+++ RedirectionDemo.java
@@ -1,16 +1,22 @@
 import java.util.*;
 public class RedirectionDemo{
   public static void main( String[]args ){
-      //use the standard input (terminal input)
-      //as the string that you read from
-      Scanner n = new Scanner( System.in );
-      //use hasNextLine()/nextLine() to loop over
-      //all of the data
-      //If you want to read the input word by word
-      //this can be replaced with hasNext() and next()
-        //do something with line
-      while(n.hasNextLine()){
-      System.out.println(n.nextLine());
+    //use the standard input (terminal input)
+    //as the string that you read from
+    Scanner line = new Scanner( System.in );
+    //use hasNextLine()/nextLine() to loop over
+    //all of the data
+    //If you want to read the input word by word
+    //this can be replaced with hasNext() and next()
+    //do something with line
+    while(line.hasNextLine()){
+      Scanner word = new Scanner(line.nextLine());
+
+      while(word.hasNext()){
+        System.out.print(word.next()+" ");
+      }
+      System.out.println();
     }
+
   }
 }
